6f412468f06e3a8733c7e31357643ad8f8ba44b3
Eudald Cardona <me@eudald.me>
2021-06-08 23:38:05 +0200

    Finish register request

--- lib/src/api/api_requests.dart
+++ lib/src/api/api_requests.dart
@@ -1,20 +1,18 @@
-import 'dart:convert';
-
-import 'package:http/http.dart';
+import 'package:rest_client/rest_client.dart' as rc;
 
 class ApiClient {
-  final String _apiUrl = "http://localhost/";
+  final String _apiUrl = "10.0.2.2";
+  final client = rc.Client();
 
   Future<bool> register(String email, String username, String password) async {
-    final url = Uri.parse('$_apiUrl/register.php:80');
-    Response response = await post(url, body: {
-        "Username": username,
-        "Password": password,
-        "Email": email,
-    });
+    var request =
+        rc.Request(url: '$_apiUrl/register.php', method: rc.RequestMethod.post);
 
-    Map<String, dynamic> json = jsonDecode(response.body);
+    var response = await client.execute(
+      authorizer: rc.TokenAuthorizer(token: 'my_token_goes_here'),
+      request: request,
+    );
 
-    return !json["error"];
+    return response.body["error"] == false;
   }
 }
