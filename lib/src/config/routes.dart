ef9e73de2ed3dc21ec6b50705dc9e73704863d66
Eudald Cardona <me@eudald.me>
2021-06-09 18:49:54 +0200

    Add missing routes

--- lib/src/config/routes.dart
+++ lib/src/config/routes.dart
@@ -1,4 +1,9 @@
 class Routes {
-  static final String logIn = "log_in";
-  static final String mainHolder = "main_holder";
+  static final String logIn = "/auth/log_in";
+  static String register = "/auth/register";
+  static String recoverPassword = "/auth/forgot_password";
+
+  static final String mainHolder = "/";
+  static final String settings = "/settings";
+  static final String info = "/settings/info";
 }
