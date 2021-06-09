f542120f40649d5a6a749e4408aa4e6d8f340b2f
Eudald Cardona <me@eudald.me>
2021-06-09 16:37:48 +0200

    Use ApiException

--- lib/src/api/api_requests.dart
+++ lib/src/api/api_requests.dart
@@ -1,12 +1,13 @@
 import 'dart:convert';
 
+import 'package:polypus_app/src/api/api_exception.dart';
 import 'package:polypus_app/src/config/globals.dart';
 import 'package:polypus_app/src/models/user.dart';
 import 'package:rest_client/rest_client.dart' as rc;
 import 'package:shared_preferences/shared_preferences.dart';
 
 class ApiClient {
-  final String _apiUrl = "10.0.2.2";
+  final String _apiUrl = "http://10.0.2.2";
   final client = rc.Client();
 
   Future<bool> register(String email, String username, String password) async {
@@ -22,7 +23,11 @@ class ApiClient {
 
     var response = await client.execute(request: request);
 
-    return response.body["error"] == false;
+    var error = response.body["error"];
+    if (error != false) {
+      throw ApiException(error);
+    }
+    return true;
   }
 
   Future<bool> login(String username, String password) async {
@@ -36,43 +41,40 @@ class ApiClient {
     );
 
     var response = await client.execute(request: request);
-
-    if (response.body["error"] == false) {
-      Globals.user = User.fromMap(response.body["body"]);
-      Globals.token = response.body["body"]["token"];
-
-      //save user to shared prefs
-      SharedPreferences.getInstance().then((prefs) {
-        prefs.setString(Globals.tokenKey, Globals.token!);
-        prefs.setString(Globals.userKey, jsonEncode(Globals.user!.toMap()));
-      });
-      return true;
+    var error = response.body["error"];
+    if (error != false) {
+      throw ApiException(error);
     }
-    return false;
+
+    Globals.user = User.fromMap(response.body["body"]);
+    Globals.token = response.body["body"]["token"];
+
+    //save user to shared prefs
+    SharedPreferences.getInstance().then((prefs) {
+      prefs.setString(Globals.tokenKey, Globals.token!);
+      prefs.setString(Globals.userKey, jsonEncode(Globals.user!.toMap()));
+    });
+    return true;
   }
 
-  Future<bool> logout() async {
+  Future<void> logout() async {
     var request = rc.Request(
       url: '$_apiUrl/logout.php',
       method: rc.RequestMethod.get,
     );
 
-    var response = await client.execute(
+    await client.execute(
       request: request,
       authorizer: rc.TokenAuthorizer(token: Globals.token ?? ""),
     );
 
-    if (response.body["error"] == false) {
-      Globals.user = null;
-      Globals.token = null;
+    Globals.user = null;
+    Globals.token = null;
 
-      //Unset shared prefs
-      SharedPreferences.getInstance().then((prefs) {
-        prefs.remove(Globals.tokenKey);
-        prefs.remove(Globals.userKey);
-      });
-      return true;
-    }
-    return false;
+    //Unset shared prefs
+    SharedPreferences.getInstance().then((prefs) {
+      prefs.remove(Globals.tokenKey);
+      prefs.remove(Globals.userKey);
+    });
   }
 }
